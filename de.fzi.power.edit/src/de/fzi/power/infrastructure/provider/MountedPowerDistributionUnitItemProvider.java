2bf2b4e47dcfb6159c172151a38d93f0f6c375fd
ChristianStier <chrisstier@gmx.de>
2015-08-27 07:49:58 +0000

    Re-generated in Luna
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Addons/Energy@28943 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- de.fzi.power.edit/src/de/fzi/power/infrastructure/provider/MountedPowerDistributionUnitItemProvider.java
+++ de.fzi.power.edit/src/de/fzi/power/infrastructure/provider/MountedPowerDistributionUnitItemProvider.java
@@ -14,101 +14,110 @@ import de.fzi.power.infrastructure.InfrastructurePackage;
 import de.fzi.power.infrastructure.MountedPowerDistributionUnit;
 
 /**
- * This is the item provider adapter for a
- * {@link de.fzi.power.infrastructure.MountedPowerDistributionUnit} object. <!-- begin-user-doc -->
+ * This is the item provider adapter for a {@link de.fzi.power.infrastructure.MountedPowerDistributionUnit} object.
+ * <!-- begin-user-doc -->
  * <!-- end-user-doc -->
- * 
  * @generated
  */
-public class MountedPowerDistributionUnitItemProvider extends PowerDistributionUnitItemProvider {
-    /**
-     * This constructs an instance from a factory and a notifier. <!-- begin-user-doc --> <!--
-     * end-user-doc -->
-     * 
-     * @generated
-     */
-    public MountedPowerDistributionUnitItemProvider(final AdapterFactory adapterFactory) {
-        super(adapterFactory);
-    }
+public class MountedPowerDistributionUnitItemProvider extends
+		PowerDistributionUnitItemProvider {
+	/**
+	 * This constructs an instance from a factory and a notifier.
+	 * <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
+	 * @generated
+	 */
+	public MountedPowerDistributionUnitItemProvider(
+			AdapterFactory adapterFactory) {
+		super(adapterFactory);
+	}
 
-    /**
-     * This returns the property descriptors for the adapted class. <!-- begin-user-doc --> <!--
-     * end-user-doc -->
-     * 
-     * @generated
-     */
-    @Override
-    public List<IItemPropertyDescriptor> getPropertyDescriptors(final Object object) {
-        if (this.itemPropertyDescriptors == null) {
-            super.getPropertyDescriptors(object);
+	/**
+	 * This returns the property descriptors for the adapted class.
+	 * <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object) {
+		if (itemPropertyDescriptors == null) {
+			super.getPropertyDescriptors(object);
 
-            this.addResourceContainerPropertyDescriptor(object);
-        }
-        return this.itemPropertyDescriptors;
-    }
+			addResourceContainerPropertyDescriptor(object);
+		}
+		return itemPropertyDescriptors;
+	}
 
-    /**
-     * This adds a property descriptor for the Resource Container feature. <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * 
-     * @generated
-     */
-    protected void addResourceContainerPropertyDescriptor(final Object object) {
-        this.itemPropertyDescriptors.add(this.createItemPropertyDescriptor(
-                ((ComposeableAdapterFactory) this.adapterFactory).getRootAdapterFactory(), this.getResourceLocator(),
-                this.getString("_UI_MountedPowerDistributionUnit_resourceContainer_feature"),
-                this.getString("_UI_PropertyDescriptor_description",
-                        "_UI_MountedPowerDistributionUnit_resourceContainer_feature",
-                        "_UI_MountedPowerDistributionUnit_type"),
-                InfrastructurePackage.Literals.MOUNTED_POWER_DISTRIBUTION_UNIT__RESOURCE_CONTAINER, true, false, true,
-                null, null, null));
-    }
+	/**
+	 * This adds a property descriptor for the Resource Container feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addResourceContainerPropertyDescriptor(Object object) {
+		itemPropertyDescriptors
+				.add(createItemPropertyDescriptor(
+						((ComposeableAdapterFactory) adapterFactory)
+								.getRootAdapterFactory(),
+						getResourceLocator(),
+						getString("_UI_MountedPowerDistributionUnit_resourceContainer_feature"),
+						getString(
+								"_UI_PropertyDescriptor_description",
+								"_UI_MountedPowerDistributionUnit_resourceContainer_feature",
+								"_UI_MountedPowerDistributionUnit_type"),
+						InfrastructurePackage.Literals.MOUNTED_POWER_DISTRIBUTION_UNIT__RESOURCE_CONTAINER,
+						true, false, true, null, null, null));
+	}
 
-    /**
-     * This returns MountedPowerDistributionUnit.gif. <!-- begin-user-doc --> <!-- end-user-doc -->
-     * 
-     * @generated
-     */
-    @Override
-    public Object getImage(final Object object) {
-        return this.overlayImage(object, this.getResourceLocator().getImage("full/obj16/MountedPowerDistributionUnit"));
-    }
+	/**
+	 * This returns MountedPowerDistributionUnit.gif.
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object getImage(Object object) {
+		return overlayImage(
+				object,
+				getResourceLocator().getImage(
+						"full/obj16/MountedPowerDistributionUnit"));
+	}
 
-    /**
-     * This returns the label text for the adapted class. <!-- begin-user-doc --> <!-- end-user-doc
-     * -->
-     *
-     * @generated
-     */
-    @Override
-    public String getText(final Object object) {
-        final String label = ((MountedPowerDistributionUnit) object).getName();
-        return label == null || label.length() == 0 ? this.getString("_UI_MountedPowerDistributionUnit_type")
-                : this.getString("_UI_MountedPowerDistributionUnit_type") + " " + label;
-    }
+	/**
+	 * This returns the label text for the adapted class. <!-- begin-user-doc --> <!-- end-user-doc
+	 * -->
+	 *
+	 * @generated
+	 */
+	@Override
+	public String getText(Object object) {
+		String label = ((MountedPowerDistributionUnit) object).getName();
+		return label == null || label.length() == 0 ? getString("_UI_MountedPowerDistributionUnit_type")
+				: getString("_UI_MountedPowerDistributionUnit_type") + " "
+						+ label;
+	}
 
-    /**
-     * This handles model notifications by calling {@link #updateChildren} to update any cached
-     * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}
-     * . <!-- begin-user-doc --> <!-- end-user-doc -->
-     * 
-     * @generated
-     */
-    @Override
-    public void notifyChanged(final Notification notification) {
-        this.updateChildren(notification);
-        super.notifyChanged(notification);
-    }
+	/**
+	 * This handles model notifications by calling {@link #updateChildren} to update any cached
+	 * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void notifyChanged(Notification notification) {
+		updateChildren(notification);
+		super.notifyChanged(notification);
+	}
 
-    /**
-     * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children that
-     * can be created under this object. <!-- begin-user-doc --> <!-- end-user-doc -->
-     * 
-     * @generated
-     */
-    @Override
-    protected void collectNewChildDescriptors(final Collection<Object> newChildDescriptors, final Object object) {
-        super.collectNewChildDescriptors(newChildDescriptors, object);
-    }
+	/**
+	 * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children
+	 * that can be created under this object.
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected void collectNewChildDescriptors(
+			Collection<Object> newChildDescriptors, Object object) {
+		super.collectNewChildDescriptors(newChildDescriptors, object);
+	}
 
 }
