adc114a22be6ca946433b551fc4ff46ed109cf0f
florian-rosenthal <florian.rosenthal@kit.edu>
2015-10-01 14:26:01 +0000

    remove some unused plugin dependencies
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Addons/Energy@29259 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- de.fzi.power.interpreter.calculators.essential/src/de/fzi/power/interpreter/calculators/essential/Activator.java
+++ de.fzi.power.interpreter.calculators.essential/src/de/fzi/power/interpreter/calculators/essential/Activator.java
@@ -3,8 +3,6 @@ package de.fzi.power.interpreter.calculators.essential;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
-import de.fzi.power.specification.resources.PowerModelConstants;
-
 public class Activator implements BundleActivator {
 
     private static BundleContext context;
@@ -15,17 +13,20 @@ public class Activator implements BundleActivator {
 
     /*
      * (non-Javadoc)
+     * 
      * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
      */
+    @Override
     public void start(BundleContext bundleContext) throws Exception {
         Activator.context = bundleContext;
-        PowerModelConstants.class.newInstance();
     }
 
     /*
      * (non-Javadoc)
+     * 
      * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
      */
+    @Override
     public void stop(BundleContext bundleContext) throws Exception {
         Activator.context = null;
     }
