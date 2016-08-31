85910c3ae6ce71cbde3930d09f299afe3a7e7a61
florian-rosenthal <florian.rosenthal@kit.edu>
2016-08-31 13:44:47 +0000

    remove unused import
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Addons/Energy@33361 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- de.fzi.power.interpreter/src/de/fzi/power/interpreter/PowerModelRegistryChangeListener.java
+++ de.fzi.power.interpreter/src/de/fzi/power/interpreter/PowerModelRegistryChangeListener.java
@@ -1,35 +1,37 @@
 package de.fzi.power.interpreter;
 
 import de.fzi.power.infrastructure.AbstractPowerConsumingResource;
-import de.fzi.power.infrastructure.PowerConsumingResource;
 import de.fzi.power.infrastructure.PowerProvidingEntity;
 import de.fzi.power.interpreter.calculators.AbstractDistributionPowerModelCalculator;
 import de.fzi.power.interpreter.calculators.IResourcePowerModelCalculator;
 
 /**
- * Listener interface to be implemented by classes which want to be notified 
- * upon changes of {@link PowerModelRegistry}s
+ * Listener interface to be implemented by classes which want to be notified upon changes of
+ * {@link PowerModelRegistry}s
  * 
  * @author Sebastian Krach
  *
  */
 public interface PowerModelRegistryChangeListener {
-   
+
     /**
      * Method is called just before a new calculator is set for a power consuming resource.
      * 
-     * @param calculator the new calculator
-     * @param affectedResource the affected resource
+     * @param calculator
+     *            the new calculator
+     * @param affectedResource
+     *            the affected resource
      */
     void resourcePowerModelChanged(IResourcePowerModelCalculator calculator,
             AbstractPowerConsumingResource affectedResource);
-    
 
     /**
      * Method is called just before a new calculator is set for a power providing entity.
      * 
-     * @param calculator the new calculator
-     * @param affectedEntity the affected resource
+     * @param calculator
+     *            the new calculator
+     * @param affectedEntity
+     *            the affected resource
      */
     void distributionPowerModelChanged(AbstractDistributionPowerModelCalculator calculator,
             PowerProvidingEntity affectedEntity);
