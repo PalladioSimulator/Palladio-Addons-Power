5cdf332f431a759a51c58d2d9412ba2d0ed425da
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2017-01-06 13:34:11 +0000

    Adapted to API change
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Addons/Energy@33967 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- de.fzi.power.interpreter/src/de/fzi/power/interpreter/calculators/TransitionStatePowerModelCalculator.java
+++ de.fzi.power.interpreter/src/de/fzi/power/interpreter/calculators/TransitionStatePowerModelCalculator.java
@@ -13,25 +13,25 @@ import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.ecore.util.EContentAdapter;
 import org.jscience.physics.amount.Amount;
 import org.palladiosimulator.measurementframework.MeasuringValue;
+import org.palladiosimulator.metricspec.BaseMetricDescription;
 import org.palladiosimulator.metricspec.MetricDescription;
-import org.palladiosimulator.metricspec.NumericalBaseMetricDescription;
 import org.palladiosimulator.metricspec.constants.MetricDescriptionConstants;
+
 import de.fzi.power.binding.TransitionState;
-import de.fzi.power.binding.impl.BindingPackageImpl;
 import de.fzi.power.infrastructure.InfrastructurePackage;
 import de.fzi.power.infrastructure.StatefulPowerConsumingResource;
 import tools.descartes.dlim.generator.ModelEvaluator;
 
 public class TransitionStatePowerModelCalculator implements IResourcePowerModelCalculator {
-    private ModelEvaluator evaluator;
-    private Unit<Power> targetUnit;
+    private final ModelEvaluator evaluator;
+    private final Unit<Power> targetUnit;
 
-    public TransitionStatePowerModelCalculator(final StatefulPowerConsumingResource resource, final TransitionState state, 
+    public TransitionStatePowerModelCalculator(final StatefulPowerConsumingResource resource, final TransitionState state,
             final ITimeProvider provider) {
         resource.eAdapters().add(new EContentAdapter() {
             @Override
-            public void notifyChanged(Notification notification) {
-                if(notification.getEventType() == Notification.SET 
+            public void notifyChanged(final Notification notification) {
+                if(notification.getEventType() == Notification.SET
                         && InfrastructurePackage.eINSTANCE.getStatefulPowerConsumingResource().isInstance(notification.getNotifier())
                         && InfrastructurePackage.eINSTANCE.getStatefulPowerConsumingResource_PowerState().equals(notification.getFeature())) {
                     transitionStart = provider.getCurrentTime();
@@ -41,16 +41,16 @@ public class TransitionStatePowerModelCalculator implements IResourcePowerModelC
         this.evaluator = new ModelEvaluator(state.getTransitionConsumption().getPowerCurve());
         this.targetUnit = state.getTransitionConsumption().getUnit();
     }
-    
+
     // TODO implement via notify
     Amount<Duration> transitionStart;
-    
+
     @Override
-    public Amount<Power> calculate(Collection<MeasuringValue> list) {
-        NumericalBaseMetricDescription timeMetric = MetricDescriptionConstants.POINT_IN_TIME_METRIC;
-        Measure<Double, Duration> curTime = list.stream().map(m -> m.<Double, Duration>getMeasureForMetric(timeMetric)).findAny().get();
-        double timeDiff = curTime.doubleValue(transitionStart.getUnit()) - transitionStart.doubleValue(transitionStart.getUnit());
-        double consumption = evaluator.getArrivalRateAtTime(timeDiff);
+    public Amount<Power> calculate(final Collection<MeasuringValue> list) {
+        final BaseMetricDescription timeMetric = MetricDescriptionConstants.POINT_IN_TIME_METRIC;
+        final Measure<Double, Duration> curTime = list.stream().map(m -> m.<Double, Duration>getMeasureForMetric(timeMetric)).findAny().get();
+        final double timeDiff = curTime.doubleValue(transitionStart.getUnit()) - transitionStart.doubleValue(transitionStart.getUnit());
+        final double consumption = evaluator.getArrivalRateAtTime(timeDiff);
         return Amount.valueOf(consumption, targetUnit);
     }
 
