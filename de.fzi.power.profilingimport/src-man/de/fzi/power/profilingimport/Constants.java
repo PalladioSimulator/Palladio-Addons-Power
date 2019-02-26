c8c48dbe1edbcd6fa6df5fcd198d07fb311ce833
FPersch <frederick.j.persch@gmail.com>
2019-02-26 11:01:36 +0100

    Restructured Palladio-Addons-Power.

--- de.fzi.power.profilingimport/src-man/de/fzi/power/profilingimport/Constants.java
+++
@@ -1,9 +0,0 @@
-package de.fzi.power.profilingimport;
-
-public class Constants {
-
-    public static final String TIME_LABEL = "t";
-    public static final String VALUE_LABEL = "value";
-    public static final String TARGET_LABEL = "TargetLevel";
-
-}
--- de.fzi.power.regression.r/src/de/fzi/power/regression/r/expressionoasis/ExportTripleProvider.java
+++
@@ -1,9 +0,0 @@
-package de.fzi.power.regression.r.expressionoasis;
-
-import org.vedantatree.expressionoasis.expressions.Expression;
-
-public interface ExportTripleProvider<T> {
-	
-	ExportTriple<T> getExportTriple(Expression expression, ExportVisitor<T> rExportVisitor);
-
-}
